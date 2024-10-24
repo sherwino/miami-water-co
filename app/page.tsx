bce470eaa9e5baddabb1030cfddc92df757af66e
Sherwin <s.garcia@applicaster.com>
2024-10-24 00:56:10 -0400

    chore: mobile landing page layout

--- app/page.tsx
+++ app/page.tsx
@@ -19,50 +19,60 @@ export default function Home() {
 
       {/* Content Grid */}
       <div className="relative z-10 h-full">
-        <div className="w-full h-full px-10">
-          <div className="grid grid-cols-1 md:grid-cols-2 w-full h-full gap-4">
+        <div className="w-full h-full px-4 md:px-10">
+          <div className="grid grid-cols-1 md:grid-cols-2 w-full h-full gap-4 md:gap-4">
             {/* Left Column - splits into two rows */}
-            <div className="grid grid-rows-2 gap-4">
-              {/* Top Cell - H3 Text */}
-              <div className="flex items-start flex-col text-center">
-              <Image src="/title-text.png" alt="Miami Water Wax Logo" width={714} height={349} className="object-contain" />
-                {/* <h3 className="text-white text-6xl font-bold w-full bg-purple-300">
-                  Behing the curtain, magic is happening
-                </h3> */}
-                {/* <p className="text-5xl">The grand unveiling of our website is coming soon</p> */}
+            <div className="grid grid-rows-2 gap-2 md:gap-4 mt-8 md:mt-0 h-[45vh] md:h-auto">
+              {/* Top Cell - Title Image */}
+              <div className="flex items-center justify-center md:items-start md:justify-start">
+                <div className="w-[250px] md:w-auto">
+                  <Image
+                    src="/title-text.png"
+                    alt="Miami Water Wax Logo"
+                    width={714}
+                    height={349}
+                    className="object-contain"
+                  />
+                </div>
               </div>
 
-              {/* Bottom Cell - Logo 1 */}
-              <div className="relative flex flex-col">
-                <Image
-                  src="/miami-water-wax-logo-alpha.png"
-                  alt="Miami Water Wax Logo"
-                  width={369}
-                  height={370}
-                  className="object-contain ml-36"
-                  priority
-                />
-                <Image
-                  src="/wax-logo-text.png"
-                  alt="Miami Water Wax Logo Text"
-                  width={724}
-                  height={177}
-                  className="object-contain -mt-8"
-                  priority
-                />
+              {/* Bottom Cell - Stacked Logos */}
+              <div className="relative flex flex-col items-center md:items-start">
+                <div className="w-[150px] md:w-auto md:ml-36">
+                  <Image
+                    src="/miami-water-wax-logo-alpha.png"
+                    alt="Miami Water Wax Logo"
+                    width={369}
+                    height={370}
+                    className="object-contain"
+                    priority
+                  />
+                </div>
+                <div className="w-[250px] md:w-auto md:-mt-8 -mt-4">
+                  <Image
+                    src="/wax-logo-text.png"
+                    alt="Miami Water Wax Logo Text"
+                    width={724}
+                    height={177}
+                    className="object-contain"
+                    priority
+                  />
+                </div>
               </div>
             </div>
 
-            {/* Right Column - full height logo */}
-            <div className="relative flex items-center justify-end w-full h-full">
-              <Image
-                src="/miami-water-badge-logo.png"
-                alt="Miami Water Badge Logo"
-                width={664}
-                height={801}
-                className="object-contain mr-24"
-                priority
-              />
+            {/* Right Column - Badge Logo */}
+            <div className="relative flex items-center justify-center md:justify-end w-full h-[45vh] md:h-full">
+              <div className="w-[250px] md:w-auto md:mr-24">
+                <Image
+                  src="/miami-water-badge-logo.png"
+                  alt="Miami Water Badge Logo"
+                  width={664}
+                  height={801}
+                  className="object-contain"
+                  priority
+                />
+              </div>
             </div>
           </div>
         </div>
